8a2ccf6e4f5b791f445e291b0aa6d0dcd483d8b8
Willian Daniel Silva <137330299+daniel02211@users.noreply.github.com>
2025-03-17 03:21:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = [];
+
+function renderTarefas() {
+  listElement.innerHTML = "";
+
+  tarefas.map((tarefa)=> {
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(tarefa);
+
+    liElement.appendChild(tarefaText);
+    listElement.appendChild(liElement);
+
+  })
+}
+
+function adicionarTarefas() {
+  if(inputElement.value === '') {
+    alert("Digite alguma cois");
+    return false;
+  }else {
+    let novaTarefa = inputElement.value;
+
+    tarefas.push(novaTarefa);
+    inputElement.value = '';
+
+    renderTarefas();
+  }
+
+}
+
+buttonElement.onclick = adicionarTarefas;
